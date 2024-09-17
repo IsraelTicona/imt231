d850f35606ad2757f73866ebdf38ae87f9475280
IsraelTicona <israel.ticona@ucb.edu.bo>
2024-09-17 10:13:19 -0400

    2024-09-17_02 segunda version incluye la variable numero_secreto

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+using namespace std;
 int main(){
 
-std::cout<<"**************************************"<<std::endl;
+cout<<"**************************************"<<endl;
 
-std::cout<<"*Bienvenido al JUEGO de la adivinanza!*"<<std::endl;
-std::cout<<"***************************************"<<std::endl;
+cout<<"*Bienvenido al JUEGO de la adivinanza!*"<<endl;
+cout<<"***************************************"<<endl;
+
+int numero_secreto = 42;
+cout<<"el numero secreto es..."<<numero_secreto<<". No lo digas a nadie!"<<endl;
 
 }
