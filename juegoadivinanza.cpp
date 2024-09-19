202a0a00e8c06ffe6fff55ebfbe918c68ccb35bd
IsraelTicona <israel.ticona@ucb.edu.bo>
2024-09-19 12:18:48 -0400

    Resolviendo error 1

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -10,4 +10,18 @@ cout<<"***************************************"<<endl;
 int numero_secreto = 42;
 cout<<"el numero secreto es..."<<numero_secreto<<". No lo digas a nadie!"<<endl;
 
+int adivina;
+cin>>adivina;
+cout<<"El valor de su numero es: "<<adivina<<endl;
+
+if(adivina == numero_secreto){
+	cout << "Felicitaciones!!...adivinó el numero secreto" <<endl;
+	}
+else if(adivina > numero_secreto){
+	cout<< "El numero ingresado es mayor que el numero secreto"<<endl;
+	}
+else{
+	cout<<"El numero ingresado es menor que el numero secreto"<<endl;
+	}
+
 }
